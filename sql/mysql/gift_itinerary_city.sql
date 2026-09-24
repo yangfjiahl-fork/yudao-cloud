aa9a5b3121ca0a01ff295f8d2fb2bed8cc994b34
yangfjiahl <383846707@qq.com>
2026-09-24 08:35:04 +0800

    feat(gift): refine itinerary city schema

--- sql/mysql/gift_itinerary_city.sql
+++ sql/mysql/gift_itinerary_city.sql
@@ -6,9 +6,10 @@ CREATE TABLE IF NOT EXISTS `gift_itinerary_city` (
   `province_id` int NOT NULL COMMENT '省级区域ID',
   `city_id` int NOT NULL COMMENT '城市区域ID',
   `city_name` varchar(64) NOT NULL COMMENT '城市名称',
-  `city_image` varchar(1024) NOT NULL DEFAULT '' COMMENT '城市图片地址',
+  `city_url` varchar(1024) NOT NULL DEFAULT '' COMMENT '城市图片地址',
   `title` varchar(128) NOT NULL DEFAULT '' COMMENT '目的地展示标题',
   `itinerary_count` int NOT NULL DEFAULT 0 COMMENT '线路数量',
+  `hot` int NOT NULL DEFAULT 0 COMMENT '热门指数',
   `sort` int NOT NULL DEFAULT 0 COMMENT '排序值',
   `creator` varchar(64) NOT NULL DEFAULT '' COMMENT '创建者',
   `create_time` datetime NOT NULL DEFAULT CURRENT_TIMESTAMP COMMENT '创建时间',
@@ -16,6 +17,5 @@ CREATE TABLE IF NOT EXISTS `gift_itinerary_city` (
   `update_time` datetime NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP COMMENT '更新时间',
   `deleted` bit(1) NOT NULL DEFAULT b'0' COMMENT '是否删除',
   PRIMARY KEY (`id`),
-  UNIQUE KEY `uk_city_id` (`city_id`),
-  KEY `idx_sort` (`sort`, `id`)
+  UNIQUE KEY `uk_city_id` (`city_id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci COMMENT='行程目的地城市';
