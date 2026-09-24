0ff1c7821710f083be4c464357fec32f881c0999
yangfjiahl <383846707@qq.com>
2026-09-24 14:04:26 +0800

    fix(gift): add itinerary day error code

--- yudao-module-gift/yudao-module-gift-api/src/main/java/cn/iocoder/yudao/module/gift/enums/ErrorCodeConstants.java
+++ yudao-module-gift/yudao-module-gift-api/src/main/java/cn/iocoder/yudao/module/gift/enums/ErrorCodeConstants.java
@@ -60,5 +60,6 @@ public interface ErrorCodeConstants {
     ErrorCode SLIDER_NOT_EXISTS = new ErrorCode(1_047_000_004, "轮播不存在");
     ErrorCode SLIDER_ITEM_NOT_EXISTS = new ErrorCode(1_047_000_005, "轮播图不存在");
     ErrorCode USER_ITINERARY_NOT_EXISTS = new ErrorCode(1_047_000_006, "用户行程不存在");
+    ErrorCode ITINERARY_DAY_NOT_EXISTS = new ErrorCode(1_047_000_007, "通用行程每日安排不存在");
 
 }
